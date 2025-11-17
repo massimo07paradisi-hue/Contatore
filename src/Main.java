10cef61a35db00d5ff28d2758dac911e9065cef7
gabri11-1 <ranigabri07@gmail.com>
2025-11-17 13:27:11 +0100

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,35 @@
-public static void main(String[] args) {
-    Counter counter = new Counter();
+public class Main {
+    public static void main(String[] args) {
+        SynchronizedBlock example = new SynchronizedBlock();
+        Counter c= new Counter();
 
-    // Create multiple threads to increment the counter
-    Thread t1 = new Thread(() -> counter.increment());
-    Thread t2 = new Thread(() -> counter.increment());
+        // Create multiple threads to increment the counter
+        Thread t1 = new Thread(() -> example.incrementSync());
+        Thread t2 = new Thread(() -> example.incrementSync());
+        Thread t3 = new Thread(() -> c.increment());
+        Thread t4 = new Thread(() -> c.increment());
 
-    t1.start();
-    t2.start();
 
-    try {
-        t1.join();
-        t2.join();
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Final count: " + c.getCount());
+        System.out.println("Final count: " + example.getCount());
     }
-
-    System.out.println("Final count: " + counter.getCount());
 }
+
+
+
