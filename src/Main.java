62f3d59d9bac0954f2daf2b6e82b13d66a68b262
Massimo <massimo07.paradisi@gmail.com>
2025-11-18 20:00:05 +0100

    nuove modifiche

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
 public static void main(String[] args) {
     Counter counter = new Counter();
+    SynchronizedBlock example = new SynchronizedBlock();
 
     // Create multiple threads to increment the counter
     Thread t1 = new Thread(() -> counter.increment());
     Thread t2 = new Thread(() -> counter.increment());
+    Thread t3 = new Thread(() -> example.increment());
+    Thread t4 = new Thread(() -> example.increment());
 
     t1.start();
     t2.start();
+    t3.start();
+    t4.start();
 
     try {
         t1.join();
         t2.join();
+        t3.join();
+        t4.join();
     } catch (InterruptedException e) {
         e.printStackTrace();
     }
 
     System.out.println("Final count: " + counter.getCount());
+    System.out.println("Final count: " + example.getCountSync());
 }
--- src/SynchronizedBlock.java
+++ src/SynchronizedBlock.java
@@ -2,11 +2,17 @@ public class SynchronizedBlock {
     private final Object lock = new Object();
     private int count = 0;
 
+    public void increment() {
+        int i = 0;
         synchronized (lock) {
+            while (i < 5000) {
                 count++;
+                i++;
+            }
         }
     }
 
+    public int getCountSync() {
         return count;
     }
-    }
+}
