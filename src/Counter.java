7b432c257da12d40e9ddf21402a912e966975bf4
Massimo <massimo07.paradisi@gmail.com>
2025-11-18 19:54:25 +0100

    nuove modifiche

--- src/Counter.java
+++ src/Counter.java
@@ -2,10 +2,18 @@ public class Counter {
     private int count = 0;
 
     public synchronized void increment() {
-        count++;
+        int i=0;
+        while(i<5000) {
+            count++;
+            i++;
+        }
+
+
+
     }
 
     public int getCount() {
+
         return count;
     }
 }
